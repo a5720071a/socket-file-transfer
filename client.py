a93445dd3e890ab07e3c6653b345e30a04b7ec76
a5720071a <a5720071a@gmail.com>
2018-04-10 19:29:59 +0700

    show progress bar on client side

--- client.py
+++ client.py
@@ -2,6 +2,8 @@
 
 import socket
 import sys
+import os
+import math
 
 s = socket.socket()
 host = socket.gethostname()
@@ -18,12 +20,13 @@ if len(sys.argv):
       filename = each
 
 s.connect((host,port))
+total_size = math.ceil(os.stat(filename).st_size / 1024.0)
 print 'Connected to server.'
 f = open(filename,'rb')
 l = f.read(1024)
 part = 1
 while(l):
-  print 'Sending data ... (%s)' % part
+  print 'Sending data ... (%s/%d)' % (part, total_size)
   s.send(l)
   l = f.read(1024)
   part += 1
